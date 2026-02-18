577e3a404fbdda16cc51c80e4446cee988ad6013
kiranvarma1309-star <kiranvarma1309@gmail.com>
2026-02-18 12:14:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
+        System.out.println("Kiran varma world");
     }
 }
